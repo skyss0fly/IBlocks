b60bbd24011367a0458fbd1b9e1240f5f037196e
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-05 20:54:47 +1000

    Update NetherSprouts.php

--- ipad54/netherblocks/blocks/NetherSprouts.php
+++ ipad54/netherblocks/blocks/NetherSprouts.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace ipad54\netherblocks\blocks;
+namespace skyss0fly\blocks\blocks;
 
-use ipad54\netherblocks\utils\CustomIds;
+use skyss0fly\blocks\utils\CustomIds;
 use pocketmine\block\Block;
 use pocketmine\block\BlockLegacyIds;
 use pocketmine\block\Flowable;

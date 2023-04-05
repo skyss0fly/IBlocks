7011689a2f6df4fff4b150c5d39f7f73c916dc23
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-05 21:08:08 +1000

    Delete ipad54/netherblocks directory

--- ipad54/netherblocks/blocks/Blackstone.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace skyss0fly\blocks\blocks;
-
-use pocketmine\block\Opaque;
-
-class Blackstone extends Opaque {
-
-}
--- ipad54/netherblocks/blocks/PolishedBlackStone.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace skyss0fly\blocks\blocks;
-
-class PolishedBlackStone extends Blackstone {
-
-}
